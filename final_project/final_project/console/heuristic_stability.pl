a63d170e781f6a5cd0fddd4e6f5c3d8e1409503c
maria-lemes <61497174+maria-lemes@users.noreply.github.com>
2021-12-02 22:17:03 +0100

    Update heuristic_stability.pl

--- final_project/final_project/console/heuristic_stability.pl
+++ final_project/final_project/console/heuristic_stability.pl
@@ -1,3 +1,9 @@
+% STABILITY HEURISTIC
+
+:- writeln('Stability heuristic has loaded.').
+% Resource : https://courses.cs.washington.edu/courses/cse573/04au/Project/mini1/RUSSIA/Final_Paper.pdf
+
+% Daprès la simplification proposée dans larticle de la source
 stabilityWeights([4,  -3,  2,  2,  2,  2, -3,  4,
                    -3, -4, -1, -1, -1, -1, -4, -3,
                    2,  -1,  1,  0,  0,  1, -1,  2,
@@ -8,28 +14,40 @@ stabilityWeights([4,  -3,  2,  2,  2,  2, -3,  4,
                    4,  -3,  2,  2,  2,  2, -3,  4]).
 
 
-stability(Board, Player, H) :- 
+heuristic_stability(Board, Player, Opponent, H) :-
     stabilityWeights(SW),
-    stability(Board, Player, H, SW).
+    heuristic_stability(Board, Player, H, SW).
 
-stability([BH|BT], Player, H, [SWH|SWT]) :-
+heuristic_stability([BH|BT], Player, H, [SWH|SWT]) :-
     HP is 0,
     HO is 0,
     switchPlayer(Player, Opponent),
-    (
-        BH==Player -> (NewHP is HP + SW, stability(BT, Player, H, SWT, NewHP, HO));
-        BH==Opponent -> (NewHO is HO + SWH, stability(BT, Player, H, SWT, HP, NewHO)).
-    ).
+    BH==Player,
+    NewHP is HP + SW,
+    heuristic_stability(BT, Player, H, SWT, NewHP, HO).
+
+
+heuristic_stability([BH|BT], Player, H, [SWH|SWT]) :-
+    HP is 0,
+    HO is 0,
+    switchPlayer(Player, Opponent),
+    BH==Opponent,
+    NewHO is HO + SWH,
+    heuristic_stability(BT, Player, H, SWT, HP, NewHO).
     
 
-
-
-stability([BH|BT], Player, H, [SWH|SWT], HP, HO) :-
+heuristic_stability([BH|BT], Player, H, [SWH|SWT], HP, HO) :-
     switchPlayer(Player, Opponent),
-    (
-        BH==Player -> (NewHP is HP + SW, stability(BT, Player, H, SWT, NewHP, HO));
-        BH==Opponent -> (NewHO is HO + SWH, stability(BT, Player, H, SWT, HP, NewHO)).
-    ).
+    BH==Player,
+    NewHP is HP + SW,
+    heuristic_stability(BT, Player, H, SWT, NewHP, HO).
 
-stability([], _, H, [], HP, HO) :-
+heuristic_stability([BH|BT], Player, H, [SWH|SWT], HP, HO) :-
+    switchPlayer(Player, Opponent),
+    BH==Opponent,
+    NewHO is HO + SWH,
+    heuristic_stability(BT, Player, H, SWT, HP, NewHO).
+
+
+heuristic_stability([], _, H, [], HP, HO) :-
     H is HP - HO.
